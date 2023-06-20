8ef27a4132def466f66b797354c9e0c2e2e3957e
Julian Stark <6523407+julian-stark@users.noreply.github.com>
2023-06-20 07:19:15 +0200

    version nr

--- stark-dynamic-template/stark-dynamic-template.php
+++ stark-dynamic-template/stark-dynamic-template.php
@@ -3,7 +3,7 @@
  * Plugin Name: Stark Dynamic Template
  * Plugin URI: https://julianstark.de/
  * Description: Stark Dynamic Template
- * Version: 2023.06.19.1
+ * Version: 2023.06.20.1
  * Author: Julian Stark
  */
 
