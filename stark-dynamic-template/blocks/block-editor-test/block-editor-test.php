9fdbcc2b6ad135874ecdba644e1eab7299243dc7
Julian Stark <6523407+julian-stark@users.noreply.github.com>
2023-06-20 07:10:45 +0200

    Minimal code for bug tracking

--- stark-dynamic-template/blocks/block-editor-test/block-editor-test.php
+++ stark-dynamic-template/blocks/block-editor-test/block-editor-test.php
@@ -24,20 +24,3 @@ if (!$block_id) {
 $content = get_post_field('post_content', $block_id);
 
 echo apply_filters('the_content', $content);
-
-return;
-
-if (is_admin()) {
-
-    echo "## is admin ##";
-
-    $post_blocks = parse_blocks(get_the_content(null, false, $block_id)); // Retrieve blocks from post content
-
-    foreach ($post_blocks as $block) {
-        $block_content = render_block($block); // Render block with inline styles
-        echo $block_content;
-    }
-
-} else {
-    echo apply_filters('the_content', $content);
-}
--- stark-dynamic-template/stark-dynamic-template.php
+++ stark-dynamic-template/stark-dynamic-template.php
@@ -9,15 +9,5 @@
 
 add_action('init', 'register_acf_blocks');
 function register_acf_blocks() {
-    register_block_type(__DIR__ . '/blocks/dynamic-template');
     register_block_type(__DIR__ . '/blocks/block-editor-test');
 }
-
-// ADD wp_block POST TYPE TO ACF POST OPTIONS
-function sd_filter_acf_get_post_types($post_types) {
-    if (!in_array('wp_block', $post_types)) {
-        $post_types[] = 'wp_block';
-    }
-    return $post_types;
-};
-add_filter('acf/get_post_types', 'sd_filter_acf_get_post_types', 10, 1);
