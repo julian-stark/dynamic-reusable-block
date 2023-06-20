3893e989d257cf2a633bea5dc5c437eabd7817e5
Julian Stark <6523407+julian-stark@users.noreply.github.com>
2023-06-20 07:24:29 +0200

    more minimal

--- stark-dynamic-template/blocks/block-editor-test/block-editor-test.php
+++ stark-dynamic-template/blocks/block-editor-test/block-editor-test.php
@@ -1,20 +1,5 @@
 <?php
 
-// Support custom "anchor" values.
-$anchor = '';
-if (!empty($block['anchor'])) {
-    $anchor = 'id="' . esc_attr($block['anchor']) . '" ';
-}
-
-// Create class attribute allowing for custom "className" and "align" values.
-$class_name = 'block-editor-test';
-if (!empty($block['className'])) {
-    $class_name .= ' ' . $block['className'];
-}
-if (!empty($block['align'])) {
-    $class_name .= ' align' . $block['align'];
-}
-
 $block_id = get_field("block_id");
 
 if (!$block_id) {
